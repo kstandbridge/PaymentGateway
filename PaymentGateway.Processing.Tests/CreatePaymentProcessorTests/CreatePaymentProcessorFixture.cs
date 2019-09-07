bbb0e6de2db5a7f635737231391484e527d911ed
kstandbridge <prepotency@gmail.com>
2019-09-07 20:10:22 +0100

    feat: CreatePaymentProcessor tests

---
+++ PaymentGateway.Processing.Tests/CreatePaymentProcessorTests/CreatePaymentProcessorFixture.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture;
+using Moq;
+using PaymentGateway.Contracts;
+using PaymentGateway.Processing.Managers;
+using PaymentGateway.Processing.Queues;
+
+namespace PaymentGateway.Processing.Tests.CreatePaymentProcessorTests
+{
+    public abstract class CreatePaymentProcessorFixture
+    {
+        protected Mock<ICommandQueue<SubmitPaymentCommand>> CommandQueueMock = new Mock<ICommandQueue<SubmitPaymentCommand>>();
+
+        protected Mock<ICreatePaymentManager> CreatePaymentManagerMock = new Mock<ICreatePaymentManager>();
+        protected Fixture Fixture = new Fixture();
+
+        protected CreatePaymentProcessor SUT;
+
+        protected CreatePaymentProcessorFixture()
+        {
+            SUT = new CreatePaymentProcessor(
+                CommandQueueMock.Object,
+                CreatePaymentManagerMock.Object);
+        }
+    }
+}
---
+++ PaymentGateway.Processing.Tests/CreatePaymentProcessorTests/StartAsync.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using Moq;
+using PaymentGateway.Contracts;
+using Xunit;
+
+namespace PaymentGateway.Processing.Tests.CreatePaymentProcessorTests
+{
+    public class StartAsync : CreatePaymentProcessorFixture
+    {
+        [Fact]
+        public async Task StartAsync_HappyPath_CallsManager()
+        {
+            // arrange
+            SubmitPaymentCommand command = Fixture.Create<SubmitPaymentCommand>();
+
+            CommandQueueMock.Setup(m => m.DequeueAsync(It.IsAny<CancellationToken>())).ReturnsAsync(command);
+            CreatePaymentManagerMock
+                .Setup(m => m.ExecuteAsync(command))
+                .Callback<SubmitPaymentCommand>(t => SUT.Dispose())
+                .Returns(Task.CompletedTask);
+
+            // act
+            await SUT.StartAsync(CancellationToken.None);
+
+            // assert
+            CreatePaymentManagerMock.Verify(m=>m.ExecuteAsync(command));
+        }
+    }
+}
